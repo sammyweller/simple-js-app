4608b839874c7c2d0fe8117e1e252ab41c87b8b0
Sammy Weller <sammymermell@gmail.com>
2023-04-12 14:28:02 -0400

    Updated script.js to begin work on the pokedex

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
-alert('Hello world');
+let pokemonList = [];
 
-let favoriteFood = 'Fries';
-document.write(favoriteFood);
+pokemonList[0] = {
+  name: 'Pikachu',
+  height: 0.4,
+  types: 'Electric',
+};
+
+pokemonList[1] = {
+  name: 'Jigglypuff',
+  height: 0.5,
+  types: [
+    'normal',
+    'fairy',
+  ]
+};
+
+pokemonList[2] = {
+  name: 'Venomoth',
+  height: 1.5,
+  types: [
+    'bug',
+    'poison',
+  ]
+};
