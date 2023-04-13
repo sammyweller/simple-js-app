63ed15f1c5fed76d54a1ac4ed5274f4354ee5a84
Sammy Weller <sammymermell@gmail.com>
2023-04-13 16:58:24 -0400

    Added conditional to highlight each pokemon based on height threshold

--- js/script.js
+++ js/script.js
@@ -18,6 +18,17 @@ let pokemonList = [
     },
 ];
 
+
+//conditional to highlight each pokemon based on height threshold//
+
 for (let i = 0; i < pokemonList.length; i++) {
-    document.write (pokemonList[i].name + " (height: " + pokemonList[i].height + ") ")
-  }
+    if (pokemonList[i].height < 1.4 && pokemonList[i].height >= 1) {
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") " + "- Cool! That's average.<br>");
+    }
+    else if (pokemonList[i].height < 0.9) {
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") " + "- That's pretty short. Cute!<br>");
+    }
+    else {
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") " + "- Wow, that's huge!<br>");
+    }
+}
