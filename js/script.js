81b0a8deb83637ee25a03ed0210477a91b4a8fb1
Sammy Weller <sammymermell@gmail.com>
2023-04-25 14:35:02 -0400

    formatted js file

--- js/script.js
+++ js/script.js
@@ -27,39 +27,38 @@ let pokemonRepository = (function () {
 
     function showDetails(pokemon) {
         loadDetails(pokemon).then(function () {
-          console.log(pokemon);
+            console.log(pokemon);
         });
     }
 
     function loadList() {
         return fetch(apiUrl).then(function (response) {
-          return response.json();
+            return response.json();
         }).then(function (json) {
-          json.results.forEach(function (item) {
-            let pokemon = {
-              name: item.name,
-              detailsUrl: item.url
-            };
-            add(pokemon);
-          });
+            json.results.forEach(function (item) {
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                add(pokemon);
+            });
         }).catch(function (e) {
-          console.error(e);
+            console.error(e);
         })
-      }
+    }
 
-      function loadDetails(item) {
+    function loadDetails(item) {
         let url = item.detailsUrl;
         return fetch(url).then(function (response) {
-          return response.json();
+            return response.json();
         }).then(function (details) {
-          // Now we add the details to the item
-          item.imageUrl = details.sprites.front_default;
-          item.height = details.height;
-          item.types = details.types;
+            item.imageUrl = details.sprites.front_default;
+            item.height = details.height;
+            item.types = details.types;
         }).catch(function (e) {
-          console.error(e);
+            console.error(e);
         });
-      }
+    }
 
     return {
         add: add,
@@ -70,7 +69,7 @@ let pokemonRepository = (function () {
     };
 })();
 
-pokemonRepository.loadList().then(function() {
+pokemonRepository.loadList().then(function () {
     pokemonRepository.getAll().forEach(function (pokemon) {
         pokemonRepository.addListItem(pokemon);
     });
